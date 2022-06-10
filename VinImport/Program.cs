5d292651dd402cc5c191fec89ccbd1cf60140308
komand33448 <90390242+komand33448@users.noreply.github.com>
2022-06-10 12:56:22 +0200

    Add files via upload

---
+++ VinImport/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Text;
+using System;
+
+namespace VinImport
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DVIService.monitorSoapClient ds = new DVIService.monitorSoapClient();
+            Console.WriteLine(ds.StockTemp().ToString("N2") + "C");
+            Console.ReadLine();
+        }
+    }
+}
+
+//http://dvimonitor.pilotdrift.dk/monitor.asmx
